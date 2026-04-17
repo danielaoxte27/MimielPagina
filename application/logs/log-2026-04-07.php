7133591b7e4e8395c622bfa5582c460c1c501e36
danielaoxte27 <danielaoxte06@gmail.com>
2026-04-17 10:38:32 -0600

    cambios

---
+++ application/logs/log-2026-04-07.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2026-04-07 20:37:29 --> 404 Page Not Found: Faviconico/index
---
+++ application/logs/log-2026-04-09.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2026-04-09 19:29:58 --> 404 Page Not Found: Faviconico/index
