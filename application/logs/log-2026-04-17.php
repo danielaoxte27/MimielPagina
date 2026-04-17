1660af87470cf20379780cfe1196422712fa4fad
danielaoxte27 <danielaoxte06@gmail.com>
2026-04-17 11:16:21 -0600

    Error

--- application/logs/log-2026-04-17.php
+++ application/logs/log-2026-04-17.php
@@ -4,3 +4,4 @@ ERROR - 2026-04-17 15:12:47 --> Severity: Warning --> Undefined variable $canale
 ERROR - 2026-04-17 15:12:47 --> Severity: Warning --> foreach() argument must be of type array|object, null given C:\laragon\www\MimielPagina\application\views\recetas\recetasformulario.php 458
 ERROR - 2026-04-17 15:12:47 --> Severity: Warning --> Undefined variable $sucursales C:\laragon\www\MimielPagina\application\views\recetas\recetasformulario.php 530
 ERROR - 2026-04-17 15:12:47 --> Severity: Warning --> foreach() argument must be of type array|object, null given C:\laragon\www\MimielPagina\application\views\recetas\recetasformulario.php 530
+ERROR - 2026-04-17 16:46:31 --> 404 Page Not Found: Faviconico/index
