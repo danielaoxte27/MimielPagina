666beeb4ed91aa3cc2a8b59bbfd817bc0577ec10
danielaoxte27 <danielaoxte06@gmail.com>
2026-04-27 21:24:06 -0600

    error

---
+++ application/logs/log-2026-04-28.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2026-04-28 02:01:14 --> 404 Page Not Found: Faviconico/index
+ERROR - 2026-04-28 03:20:58 --> Severity: error --> Exception: Unknown column 'NULL' in 'field list' C:\laragon\www\MimielPagina\system\database\drivers\mysqli\mysqli_driver.php 305
