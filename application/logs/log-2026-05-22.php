544154bf052c7bc49e3784d17c68890f02ace334
danielaoxte27 <danielaoxte06@gmail.com>
2026-05-22 18:16:56 -0600

    error

---
+++ application/logs/log-2026-05-22.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2026-05-22 23:31:46 --> 404 Page Not Found: Principal/MIEL
+ERROR - 2026-05-22 23:31:57 --> 404 Page Not Found: Productos/miel
